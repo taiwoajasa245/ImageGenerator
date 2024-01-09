38e19e148f12fa154c79ae73921ce9938d66bd5d
Ajasa Taiwo <ajasataiwo45@gmail.com>
2024-01-09 02:05:52 +0100

    added loader

--- public/script.js
+++ public/script.js
@@ -1,20 +1,35 @@
 const searchBtn  = document.getElementById('search_btn');
 const searchBox = document.getElementById('search_box'); 
+const images = document.getElementById('myImages'); 
+
+
+// hide imgae 
+function HideImage() {    
+    document.getElementById("loader").style.display = "flex";
+    images.style.display = "none";
+}
+// show images after loder
+function showImage() { 
+    document.getElementById("loader").style.display = "none";
+    images.style.display = "flex";
+}
+
 
 searchBtn.addEventListener('click', async () => {
     if( searchBox.value === ""){ 
         alert('Enter a value to search For an image');
     }else{ 
         try {
-        
+            HideImage(); 
             // Make a request to your server to get images
                     const response = await fetch(`http://localhost:8080/getImages?q=${searchBox.value}`);
                     const data = await response.json();
     
                     // Handle the images data
-                    const imageContainer = document.querySelector('.gallery-image');
+                    const imageContainer = images
                     imageContainer.innerHTML = ''; // Clear previous images
-    
+
+
                     if (data.hits && data.hits.length > 0) {
                         data.hits.forEach(image => {
                             const imgElement = document.createElement('img');
@@ -25,6 +40,8 @@ searchBtn.addEventListener('click', async () => {
                     } else {
                         imageContainer.innerHTML = '<p>No images found.</p>';
                     }
+
+                    showImage();
     
                 } catch (error) {
                     console.error(error);
